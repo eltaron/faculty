d49a1fb95c1f8a9bb946b0ac9f34e04528c80908
eltaron <73391514+eltaron@users.noreply.github.com>
2020-12-07 14:25:27 +0200

    ahmed_mod

--- js/index.js
+++ js/index.js
@@ -2,3 +2,15 @@ $('#container').mixItUp();
 $('.shuffle li').click(function() {
     $(this).addClass('select').siblings().removeClass('select');
 });
+//scroll top     
+var scrollButton = $("#scroll-top");
+$(window).scroll(function () {
+if ( $(this).scrollTop() >= 600 ) { 
+    scrollButton.show();
+} else {
+    scrollButton.hide();
+}
+});
+scrollButton.click(function () {
+    $("html,body").animate({ scrollTop: 0}, 600);
+});
