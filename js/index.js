fb2a2c1b7290aef2d56e19a8cfd22b81c6835878
eltaron <73391514+eltaron@users.noreply.github.com>
2020-12-09 21:38:41 +0200

    account

--- js/index.js
+++ js/index.js
@@ -14,3 +14,12 @@ if ( $(this).scrollTop() >= 600 ) {
 scrollButton.click(function () {
     $("html,body").animate({ scrollTop: 0}, 600);
 });
+//select
+$("select.main_select").change(function(){
+    $selected = $(this).children("option:selected").data('show');
+    $("#"+ $selected).show().siblings('select').hide();
+});
+//livepreview
+$('.live').keyup(function () {
+    $($(this).data('class')).text($(this).val());
+});
